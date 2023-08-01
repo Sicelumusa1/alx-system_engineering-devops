2feee42f6abf72125530c11d69a3e00991fe76f2
Musa <musaqwabe@gmail.com>
2023-08-01 04:32:02 -0700

    Added file 0-2 and readme

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+def school_match(string)
+    patten = /hb*tn/
+
+    match_found = string.match(patten)
+
+    if match_found
+      puts match_found[0]
+    else
+      puts ""
+    end
+end
+
+input = ARGV[0]
+
+school_match(input)
