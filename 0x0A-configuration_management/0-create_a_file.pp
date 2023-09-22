baee45d0b8fc490c96b01581a5eca0bfb361ba1d
Musa <musaqwabe@gmail.com>
2023-09-22 15:22:22 -0700

    Added file documentation

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,2 +1,10 @@
-file { '/tmp/school': ensure => 'file', mode => '0744', owner => 'www-data', group => 'www-data', content=> 'I love Puppet',
+#  Create a file in /tmp with 0744 permissions, www-data as owner and group
+#  and contents 'I love Puppet'
+
+file { '/tmp/school': 
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
