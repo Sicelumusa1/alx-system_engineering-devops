27844c1dd13ab94cde0d37ec241d97ec1a56fd6f
Musa <musaqwabe@gmail.com>
2023-09-23 09:38:18 -0700

    Removed trailling white spaces

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 #  Create a file in /tmp with 0744 permissions, www-data as owner and group and contents 'I love Puppet'
 
-file { '/tmp/school': 
+file { '/tmp/school':
   ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
