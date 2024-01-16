f5c92b8dc957bcf0206d2720de8da7dd039c56d7
Sicelumusa <musaqwabe@gmail.com>
2024-01-16 12:10:05 -0800

    changed user agent

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -24,18 +24,15 @@ def recurse(subreddit, hot_list=[]):
         it the subreddit is invald or the request fails.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-               "AppleWebKid/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 "
-               "Safari/537.36"}
-    params = {"after": after} if after else {}
-    response = requests.get(url, headers=headers, params=params)
+    headers = {"User-Agent": "Codig in Python"}
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()["data"]
         hot_list += [post["data"]["title"] for post in data["children"]]
-        after = data["after"]
+        after = data.get["after"]
         if after:
-            recurse(subreddit, hot_list, after)
+            recurse(subreddit, hot_list)
         return hot_list
     else:
         print(None)
